cf8e6de47476972cff5e40a5a7fdeced8c62abcf
rahul <rahul@git>
2012-06-26 22:31:44 -0700

    adding gpl

--- src/com/irahul/worldclock/test/WorldClockDataTest.java
+++ src/com/irahul/worldclock/test/WorldClockDataTest.java
@@ -1,5 +1,26 @@
 package com.irahul.worldclock.test;
-
+/*
+ * Copyright (C) 2012 Rahul Agarwal
+ *
+ * This file is part of the World Clock
+ * World Clock is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * World Clock is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with World Clock.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * Placeholder file
+ * @author rahul
+ *
+ */
 public class WorldClockDataTest {
 
 }
